5bbbe5729711bd021aa4bdb99386e5ea3373649c
thefakejj <jasper.laurila@gmail.com>
2023-11-12 16:31:36 +0200

    muokattu project reader ja project lopulliseen muotoon, poistettu ylimääräinen koodi

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,29 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, proj_license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.proj_license = proj_license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    def _bullet_point_list(self, data):
+        if len(data) > 0:
+            bullet_point = ""
+            for element in data:
+                bullet_point += f"\n- {element}"
+            return bullet_point
+        return "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.proj_license or '-'}"
+            f"\n"
+            f"\nAuthors: {self._bullet_point_list(self.authors)}"
+            f"\n"
+            f"\nDependencies: {self._bullet_point_list(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies: {self._bullet_point_list(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -8,20 +8,18 @@ class ProjectReader:
         self._url = url
 
     def get_project(self):
-        # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
 
         parsed_toml = toml.loads(content)
 
         tool = parsed_toml["tool"]
-
         poetry = tool["poetry"]
 
         project_name = poetry["name"]
         project_description = poetry["description"]
+        project_license = poetry["license"]
+        project_authors = poetry["authors"]
         project_dependencies = poetry["dependencies"]
         project_dev_dependencies = poetry["group"]["dev"]["dependencies"]
 
-
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(project_name, project_description, project_dependencies, project_dev_dependencies)
+        return Project(project_name, project_description, project_license, project_authors, project_dependencies, project_dev_dependencies)
