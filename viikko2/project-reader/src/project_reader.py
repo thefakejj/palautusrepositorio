ee4c54162b6c03c9a714e1e76c7759a961bf7b8c
thefakejj <jasper.laurila@gmail.com>
2023-11-12 16:14:39 +0200

    poistettu ylimääräinen koodi project readeristä

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -10,25 +10,13 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        # print(content)
 
         parsed_toml = toml.loads(content)
 
         tool = parsed_toml["tool"]
-        build_system = parsed_toml["build-system"]
+
         poetry = tool["poetry"]
 
-        # print(tool.keys())
-        # print(build_system.keys())
-        # print(poetry.keys())
-
-        #print(poetry)
-        print("\n")
-        print(poetry["group"])
-        # print("tool:", tool)
-        # print("\n")
-        # print("build-system:", build_system)
-        # print("\n")
         project_name = poetry["name"]
         project_description = poetry["description"]
         project_dependencies = poetry["dependencies"]
@@ -36,5 +24,4 @@ class ProjectReader:
 
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        # return Project("Test name", "Test description", [], [])
         return Project(project_name, project_description, project_dependencies, project_dev_dependencies)
