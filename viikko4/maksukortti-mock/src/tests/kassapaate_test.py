287e7b481093d89565743cabc68fef8df636fd4d
thefakejj <jasper.laurila@gmail.com>
2023-11-26 15:08:37 +0200

    NELJÄS testi toimii, kolmas ei

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,23 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    # def test_kortille_ladataan_jos_positiivinen(self):
+        # maksukortti_mock = Mock()
+
+        # self.kassa.lataa(maksukortti_mock, 5)
+
+        # maksukortti_mock.lataa.assert_called_with(5)
+        # print("tässä maksukortti_mock.saldo:", maksukortti_mock.saldo)
+
+        # self.assertAlmostEqual(maksukortti_mock.saldo, 5)
+
+    def test_kortille_ei_ladata_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()
+        # print("tässä maksukortti_mock.saldo:", maksukortti_mock.saldo)
+
+        #self.assertAlmostEqual(maksukortti_mock.saldo, 5)
