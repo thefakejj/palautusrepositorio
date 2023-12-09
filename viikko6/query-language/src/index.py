5a38ab00239dd6fe6e5c84c136a0092fc867f4eb
thefakejj <jasper.laurila@gmail.com>
2023-12-09 20:12:59 +0200

    matcher for All added

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,28 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, All, Not
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    # matcher = And(
+    #     HasAtLeast(5, "goals"),
+    #     HasAtLeast(20, "assists"),
+    #     PlaysIn("PHI")
+    # )
+    matcher = All(
+    )
+
+    # matcher = And(
+    # Not(HasAtLeast(2, "goals")),
+    # PlaysIn("NYR")
+    # )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
