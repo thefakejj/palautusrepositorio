38b7d8478d8723fdda696f33719ce73fdb9eff29
thefakejj <jasper.laurila@gmail.com>
2023-12-14 19:50:43 +0200

    luo peli toteutettu sanakirjalla

--- viikko7/kivi-paperi-sakset/src/luo_peli.py
+++ viikko7/kivi-paperi-sakset/src/luo_peli.py
@@ -3,11 +3,9 @@ from kps_tekoaly import KPSTekoaly
 from kps_parempi_tekoaly import KPSParempiTekoaly
 
 def luo_peli(tyyppi):
-    if tyyppi == 'a':
-        return KPSPelaajaVsPelaaja()
-    if tyyppi == 'b':
-        return KPSTekoaly()
-    if tyyppi == 'c':
-        return KPSParempiTekoaly()
-
-    return None
+    pelit = {
+        "a": KPSPelaajaVsPelaaja,
+        "b": KPSTekoaly,
+        "c": KPSParempiTekoaly
+    }
+    return pelit[tyyppi]()
