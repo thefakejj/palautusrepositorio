50e1c89c6152a420156cbad65903f1ca19880a53
thefakejj <jasper.laurila@gmail.com>
2023-12-14 17:02:08 +0200

    indexissä vain yksi if

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,6 +1,7 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
+# from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+# from kps_tekoaly import KPSTekoaly
+# from kps_parempi_tekoaly import KPSParempiTekoaly
+from luo_peli import luo_peli
 
 
 def main():
@@ -14,27 +15,14 @@ def main():
 
         vastaus = input()
 
-        if vastaus.endswith("a"):
+        vastauksen_loppu = vastaus[-1]
+
+        if vastaus[-1] in ["a", "b", "c"]:
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
+            peli = luo_peli(vastauksen_loppu)
+            peli.pelaa()
         else:
             break
 
